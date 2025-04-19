7b97b71b900e36b138dee7b8832be328e7091dbc
Jouni Kiviperä <jounikivipera@gmail.com>
2025-04-19 10:24:28 +0300

    päivityksiä

--- jounicvsite/settings.py
+++ jounicvsite/settings.py
@@ -102,13 +102,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'fi'         # ← muutetaan suomeksi
+TIME_ZONE = 'Europe/Helsinki'  # ← Suomen aikavyöhyke
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
