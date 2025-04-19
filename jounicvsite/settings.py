970b46fdd06bd7f9b69aee464b1eb83b903a0643
Jouni Kiviperä <jounikivipera@gmail.com>
2025-04-19 10:28:03 +0300

    Update settings.py

--- jounicvsite/settings.py
+++ jounicvsite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()  # Lataa ympäristömuuttujat .env-tiedostosta 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-895ltz6z-kc*g7*#*l0ckf%w@$u7stxbr@w*+2e0#h4#$fmhuj'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
