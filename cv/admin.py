5b9939b0feb6e9f870cc796d32741105d795e00c
Jouni Kiviperä <jounikivipera@gmail.com>
2025-04-24 09:20:04 +0300

    päivityksiä

--- cv/admin.py
+++ cv/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TietojaMinusta, Tyokokemus, Koulutus, Taidot
 
 # Register your models here.
+admin.site.register(TietojaMinusta)
+admin.site.register(Tyokokemus)
+admin.site.register(Koulutus)
+admin.site.register(Taidot) 
--- cv/models.py
+++ cv/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class TietojaMinusta(models.Model):
+  otsikko = models.CharField(max_length=100)
+  kuvaus = models.TextField()
+  kuva = models.ImageField(upload_to='kuvat/')
+  
+  
+class Tyokokemus(models.Model):
+    yritys = models.CharField(max_length=100)
+    tehtava = models.CharField(max_length=100)
+    kuvaus = models.TextField()
+    alkupvm = models.DateField()
+    loppupvm = models.DateField()
+    
+class Koulutus(models.Model):
+    oppilaitos = models.CharField(max_length=100)
+    tutkinto = models.CharField(max_length=100)
+    kuvaus = models.TextField()
+    aloitusvuosi = models.DateField()
+    valmistumisvuosi = models.DateField()
+
+class Taidot(models.Model):
+    taido = models.CharField(max_length=100)
+    taso = models.CharField(max_length=100)
