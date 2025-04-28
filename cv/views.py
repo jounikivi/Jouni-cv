f61641d6f57b9c70f38b0392accb2b365a5dda26
Jouni Kiviperä <jounikivipera@gmail.com>
2025-04-28 08:34:48 +0300

    päivityksiä

--- cv/views.py
+++ cv/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import TietojaMinusta
 
 def home(request):
+    tietoja = TietojaMinusta.objects.all()
     return render(request, 'cv/home.html', {'tietoja': tietoja})
