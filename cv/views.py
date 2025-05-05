0dffe529aac6360381685a2c78cc095d7b62f247
Jouni Kiviperä <jounikivipera@gmail.com>
2025-05-05 08:29:07 +0300

    päivityksiä

--- cv/views.py
+++ cv/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import TietojaMinusta
+from .models import TietojaMinusta, Tyokokemus
 
 def home(request):
     tietoa = TietojaMinusta.objects.first()
+    tyokokemus = Tyokokemus.objects.all().order_by('-alkupvm')
     return render(request, 'cv/home.html', {'tietoa': tietoa})
