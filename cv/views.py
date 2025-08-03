b9b690c1e85d1cf2f4440214d9a6a2400dd2c6bc
Jouni Kiviperä <jounikivipera@gmail.com>
2025-08-03 13:49:15 +0300

    Query education and skills

--- cv/views.py
+++ cv/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from .models import TietojaMinusta, Tyokokemus
+from .models import TietojaMinusta, Tyokokemus, Koulutus, Taidot
 
 
 def home(request):
     tietoa = TietojaMinusta.objects.first()
     tyokokemukset = Tyokokemus.objects.all().order_by('-alkupvm')
+    koulutukset = Koulutus.objects.all().order_by('-aloitusvuosi')
+    taidot = Taidot.objects.all().order_by('-taso')
     return render(request, 'cv/home.html', {
         'tietoa': tietoa,
-        'tyokokemukset': tyokokemukset
+        'tyokokemukset': tyokokemukset,
+        'koulutukset': koulutukset,
+        'taidot': taidot,
     })
 
